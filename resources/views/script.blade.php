c7e0e622071713fb10633d8bd8e5e2f1cc90e8a3
Kristoffer Tonning <tonning@gmail.com>
2022-03-09 12:39:50 +0100

    Only load script if app is enabled

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,4 +1,8 @@
- <!-- Optimizely -->
-    <link rel="preload" href="//cdn.optimizely.com/js/{{settings('Astrogoat\\Optimizely\\Settings\\OptimizelySettings', 'account_id')}}.js" as="script">
+@php($accountId = settings('Astrogoat\\Optimizely\\Settings\\OptimizelySettings', 'account_id'))
+
+@if(Astrogoat\Optimizely\Settings\OptimizelySettings::isEnabled() && ! blank($accountId))
+    <!-- Optimizely -->
+    <link rel="preload" href="//cdn.optimizely.com/js/{{ $accountId }}.js" as="script">
     <link rel="preconnect" href="//logx.optimizely.com">
-    <script src="https://cdn.optimizely.com/js/{{settings('Astrogoat\\Optimizely\\Settings\\OptimizelySettings', 'account_id')}}.js"></script>
+    <script src="https://cdn.optimizely.com/js/{{ $accountId }}.js"></script>
+@endif
